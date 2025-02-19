6181d2de129f429cab3f488c83bae657e92e5576
YabseraBogale <yabserapython@gmail.com>
2025-02-19 10:58:15 +0300

    ok

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def resume():
     filename="resume.pdf"
     return send_from_directory(directory,filename,as_attachment=True)
 
-
+@app.route("/blog/<name>")
+def blog(name):
+    return render_template(name)
 
 if __name__=="__main__":
     app.run(debug=True)
