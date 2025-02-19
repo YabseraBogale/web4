3a8ec7f3a78f324df1e7674fcd8febfe4450c39c
YabseraBogale <yabserapython@gmail.com>
2025-02-19 10:30:04 +0300

    ok

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask,render_template,url_for,send_from_directory
 
+
+app=Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/resume")
+def resume():
+    directory="static/files"
+    filename="resume.pdf"
+    return send_from_directory(directory,filename,as_attachment=True)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
