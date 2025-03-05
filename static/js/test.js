92b2220d05974787481317dfc327db97794b3562
YabseraBogale <yabserapython@gmail.com>
2025-03-05 19:10:46 +0300

    ok

--- static/js/test.js
+++ static/js/test.js
@@ -1,18 +1,21 @@
+let number=0
 fetch("http://127.0.0.1:5000/exit_exam_question_answer_api")
     .then((response)=> response.json())
     .then(data=>{
         const question=document.getElementById("question")
-        question.innerText=data[0][0]
+        question.innerText=data[number][0]
         let choice_a = document.getElementById("a")
         let choice_b = document.getElementById("b")
         let choice_c = document.getElementById("c")
         let choice_d = document.getElementById("d")
-        let anwser = document.querySelector("anwser")
-        let explanation=document.querySelector("explanation")
-        choice_a.innerText=data[0][1]
-        choice_b.innerText=data[0][2]
-        choice_c.innerText=data[0][3]
-        choice_d.innerText=data[0][4]
+        let anwser = document.getElementById("answer")
+        let explanation=document.getElementById("explanation")
+        choice_a.innerText=data[number][1]
+        choice_b.innerText=data[number][2]
+        choice_c.innerText=data[number][3]
+        choice_d.innerText=data[number][4]
+        anwser.innerText="Answer: "+data[number][5]
+        explanation.innerText="Explanation: "+data[number][6]
         
     })
     .catch(err=>{
