74cbfe26f1db8d9ac56b4612272960f429a8bb05
YabseraBogale <yabserapython@gmail.com>
2025-03-29 17:56:00 +0300

    ok

--- static/js/balance_sheet.js
+++ static/js/balance_sheet.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const nameCompany=document.getElementById("namecompany") 
+
+const addCompany=document.getElementById("AddCompany")
+
+const list =document.getElementById("list")
+
+addCompany.addEventListener("click",function(){
+    const company=document.createElement("ul")
+    
+})
