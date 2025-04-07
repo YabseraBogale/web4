a7bcfe470883102ebdda15591ff9be07920f7a3b
YabseraBogale <yabserapython@gmail.com>
2025-04-07 20:29:41 +0300

    ok

---
+++ blog_template_maker.py
@@ -0,0 +1,44 @@
+import os
+
+name=input("Enter template name: ")
+
+
+directory="templates"
+
+filepath=os.path.join(directory,f"{name}.html")
+
+os.makedirs(directory,exist_ok=True)
+
+with open(filepath,'w') as f:
+    f.write("""
+{% extends "base.html" %}
+
+{% block headtitle %}
+      
+{% endblock %}
+
+{% block title %}
+     
+{% endblock %}
+
+{% block intro %}
+
+{% endblock %}
+
+{% block content %}
+
+{% endblock %}
+
+{% block conclusion %}
+
+{% endblock %}
+
+{% block prev %}
+    <a href="">Prev</a>
+{% endblock %}
+
+{% block next %}
+    <a href="">Next</a>
+{% endblock %}
+        """
+    )
