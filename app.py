9b5040e3ac3a810b74e270506845f3fe767078e8
YabseraBogale <yabserapython@gmail.com>
2025-04-18 12:20:30 +0300

    looks everything is right now to test

--- app.py
+++ app.py
@@ -53,8 +53,14 @@ def random_software_fact():
     facts=exit_exam.random_software_fact()
     return render_template("dictionary_for_software.html",Facts=facts)
 
-@app.route("/inflation_calculator")
+@app.route("/inflation_calculator",methods=["GET","POST"])
 def inflation_calculator():
+    if request.method=="POST":
+        country=request.form.get("country")
+        year_at=request.form.get("year_at")
+        year_in=request.form.get("year_in")
+        amount=request.form.get("amount")
+        return request.form.get("country")
     return render_template("inflation_calculator.html")
 
 if __name__=="__main__":
