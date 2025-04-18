51a4a52c28eb4ac84f4f7aae374f8a80d587dea4
YabseraBogale <yabserapython@gmail.com>
2025-04-18 12:27:20 +0300

    looks everything is right now to test

---
+++ Database/InflationCalculator.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class inflation_calculator():
+
+    def __init__(self):
+        try:
+            self.connection=sqlite3.Connection("database.db",check_same_thread=False)
+            self.pointer=self.connection.cursor()
+        except Exception as e:
+            print(e)
+
+    def calculator(self,country,year_in,year_at,amount):
+        try:
+            """
+                Adjusted Amount = Initial Amount * (CPI in Target Year / CPI in Initial Year)
+
+                Where:
+                    Initial Amount: The amount of money you start with.
+                    CPI in Initial Year: The price index value for the year you are starting from.
+                    CPI in Target Year: The price index value for the year you want to adjust the money to.
+                    Adjusted Amount: The equivalent value of the Initial Amount in the Target Year's currency value.
+            """
+        except Exception as e:
+            return str(e)
